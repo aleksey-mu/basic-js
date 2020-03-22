dd069a18eb9cc7d08eda456fe57794284767d816
aleksey_mu <pacuklex@mail.ru>
2020-03-22 22:25:07 +0300

    feat: transf array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,19 +3,19 @@ module.exports = function transform(arr) {
     const resultArr = [];
     const arrLength = arr.length
     for (let i = 0; i < arrLength; i += 1) {
-        if (arr[i] == '--discard-next') {
+        if (arr[i] === '--discard-next') {
             if (i + 1 <= arrLength - 1) {
                 i += 1;
             }
-        } else if (arr[i] == '--discard-prev') {
+        } else if (arr[i] === '--discard-prev') {
             if (i - 1 >= 0) {
                 resultArr.pop();
             }
-        } else if (arr[i] == '--double-next') {
+        } else if (arr[i] === '--double-next') {
             if (i + 1 <= arrLength - 1) {
                 resultArr.push(arr[i + 1]);
             }
-        } else if (arr[i] == '--double-prev') {
+        } else if (arr[i] === '--double-prev') {
             if (i - 1 >= 0) {
                 resultArr.push(arr[i - 1]);
             }
